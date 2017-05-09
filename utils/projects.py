40a236474b48edfcb1b92e5c1d6fbd9e8098ef45
Alvaro del Castillo <acs@bitergia.com>
2017-05-09 07:12:13 +0200

    [utils] Add utils for testing pathfinder lib for creating projects files.
    In future iterations the projects will be created in bestiary database.

---
+++ utils/projects.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#
+# Copyright (C) 2017 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#   Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+
+import json
+import logging
+import shutil
+
+logger = logging.getLogger(__name__)
+
+class Projects():
+    def __init__(self, projects_file):
+        self.projects_file = projects_file
+        with open(self.projects_file, "r") as fprojects:
+            self.projects = json.load(fprojects)
+
+    def __check_project(self, project):
+        if project not in self.projects:
+            msg_error = "Can't find %s in %s" % (project, self.projects.keys())
+            raise RuntimeError(msg_error)
+
+    def update_project_repos(self, project, data_source, repos):
+        self.__check_project(project)
+
+        self.projects[project][data_source] = repos
+        self.projects[project][data_source] = list(set(self.projects[project][data_source]))
+
+    def set_project_repos(self, project, data_source, repos):
+        self.__check_project(project)
+
+        self.projects[project][data_source] = repos
+
+    def get_project_repos(self, project, data_source):
+        self.__check_project(project)
+
+        return self.projects[project][data_source]
+
+    def get_projects(self):
+        return self.projects.keys()
+
+    def get_project_data_sources(self, project):
+        self.__check_project(project)
+
+        return self.projects[project].keys()
+
+    def dump(self):
+        # Backup the projects file
+        shutil.copy(self.projects_file, self.projects_file + ".bak")
+        with open(self.projects_file, "w") as fprojects:
+            json.dump(self.projects, fprojects, sort_keys=True, indent=4)
+
+        logging.info("Projects file updated %s", self.projects_file)
---
+++ utils/update_projects.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#
+# Copyright (C) 2017 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#   Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+
+""" Sample script for managing projects from GitHub repositories """
+
+import argparse
+import logging
+import sys
+
+sys.path.insert(0, '..')
+from pathfinder.repositories.github import ReposGitHub
+from projects import Projects
+
+logger = logging.getLogger(__name__)
+
+def config_logging(debug):
+    if debug:
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+        logging.debug("Debug mode activated")
+    else:
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+
+def get_params():
+    """Parse command line arguments"""
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-g', '--debug', dest='debug', action='store_true')
+    parser.add_argument('-t', '--token', dest='token', help="GitHub token")
+    parser.add_argument('-o', '--owners', dest='owners', nargs='*',
+                        help='GitHub owners to get repos from')
+    parser.add_argument('-r', '--repos', dest='repos', nargs='*',
+                        help='GitHub repos to include')
+    parser.add_argument('-b', '--blacklist', dest='blacklist', nargs='*',
+                        help='GitHub repos to exclude')
+    parser.add_argument('-p', '--project', dest='project',
+                        help='Project repos to be updated')
+    parser.add_argument('--projects-file', dest='projects_file',
+                        help='Projects file to be updated')
+    parser.add_argument('-f', '--forks', dest='forks', action='store_true',
+                        help='Include forked repos')
+
+    args = parser.parse_args()
+
+    if not args.owners or not args.token or not args.projects_file or not args.project:
+        parser.error("token, owner, project and projects file params must be provided.")
+        sys.exit(1)
+
+    return args
+
+
+if __name__ == '__main__':
+
+    args = get_params()
+
+    config_logging(args.debug)
+
+    # Just github in this first iteration
+    data_source = "github"
+
+    # Load the projects to be updated
+    project = args.project
+    projects = Projects(args.projects_file)
+
+    # Retrieve all the repositories
+    repos = ReposGitHub(args.owners, args.token)
+    repos_list = []
+    for repo in repos.get_list():
+        repos_list.append(repo["html_url"])
+
+    # args.blacklist, args.repos, args.forks
+
+    logger.debug("Added repos: %s", set(repos_list) - set(projects.get_project_repos(project, 'github')))
+    logger.debug("Removed repos: %s", set(projects.get_project_repos(project, 'github')) - set(repos_list))
+
+    # projects.set_project_repos(project, data_source, repos_list)
+    projects.update_project_repos(project, data_source, repos_list)
+    projects.dump()
