890db79c3a2fc9e50f804357530b81bb7b4853b6
Alvaro del Castillo <acs@bitergia.com>
2017-12-07 08:43:22 +0100

    [model] Add created_at and updated_at fields, and add org to Project
    All objects has now the created_at and updated_at fields.
    The Project object now includes the org related so the projects
    are now isolated in each org (there are no cross-org projects).

--- django_bestiary/projects/models.py
+++ django_bestiary/projects/models.py
@@ -3,16 +3,20 @@ from django.db import models
 
 # Create your models here.
 class DataSource(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     name = models.CharField(max_length=200, unique=True)
-    # creation_date = models.DateTimeField('date creation')
 
     def __str__(self):
         return self.name
 
 
 class Repository(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     name = models.CharField(max_length=200)
-    # creation_date = models.DateTimeField('date creation')
     # Relations
     data_source = models.ForeignKey(DataSource, on_delete=models.CASCADE)
 
@@ -24,8 +28,9 @@ class Repository(models.Model):
 
 
 class RepositoryView(models.Model):
-    # creation_date = models.DateTimeField('date creation')
-    # update_date = models.DateTimeField('date update')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     filters = models.CharField(max_length=400)  # A View is a Filter
     # Relations
     # Base Repository from which to create the View
@@ -36,23 +41,29 @@ class RepositoryView(models.Model):
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    name = models.CharField(max_length=200)
     meta_title = models.CharField(max_length=200)
-    # creation_date = models.DateTimeField('date creation')
-    # update_date = models.DateTimeField('date update')
     # Relations
     rep_views = models.ManyToManyField(RepositoryView)
     # https://docs.djangoproject.com/en/1.11/ref/models/fields/#foreignkey
     subprojects = models.ManyToManyField("Project")
+    org = models.ForeignKey("Organization", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('name', 'org')
 
     def __str__(self):
         return self.name
 
 
 class Organization(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     name = models.CharField(max_length=200, unique=True)
-    # creation_date = models.DateTimeField('date creation')
-    # update_date = models.DateTimeField('date update')
     # Relations
     projects = models.ManyToManyField(Project)
     suborgs = models.ManyToManyField("Organization")
