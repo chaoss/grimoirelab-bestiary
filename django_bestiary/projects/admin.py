90cd77b2b9454c5e3e971ed0779bec57d748b42c
Alvaro del Castillo <acs@bitergia.com>
2017-12-10 00:42:31 +0100

    [admin] Add filter per data source type for repositories in admin interface

--- django_bestiary/projects/admin.py
+++ django_bestiary/projects/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from . import models
 
+
+class RepositoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'data_source_type',)
+    list_filter = ('data_source_type',)
+
 # Register your models here.
+
 
 admin.site.register(models.Organization)
 admin.site.register(models.Project)
-admin.site.register(models.Repository)
+admin.site.register(models.Repository, RepositoryAdmin)
 admin.site.register(models.DataSource)
 admin.site.register(models.DataSourceType)
