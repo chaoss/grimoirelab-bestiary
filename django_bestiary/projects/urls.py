40fe89e4ad7f4ca57da8c26af009093ff950bb36
Alvaro del Castillo <acs@bitergia.com>
2018-01-11 19:51:00 +0100

    [editor] Use Django Forms + Bootstrap to build the Editor GUI
    In the middle of implementing the forms strategy in Bestiary, mixing
    Django Forms with Bootstrap styling.

--- django_bestiary/projects/urls.py
+++ django_bestiary/projects/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^import/$', views.import_from_file),
     url(r'^export/$', views.export_to_file),
     url(r'^editor/$', views.editor, name='editor'),
+    url(r'^edit_ecosystem/$', views.edit_ecosystem, name='editor'),
     url(r'^$', views.index, name='index'),
 ]
