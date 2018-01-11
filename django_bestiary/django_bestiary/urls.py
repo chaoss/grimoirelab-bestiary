40172e3ec70e0cc395a45a87600d3d0baaf4d456
Alvaro del Castillo <acs@bitergia.com>
2018-01-11 14:40:09 +0100

    [editor] Add non functional bestiary editor

--- django_bestiary/django_bestiary/urls.py
+++ django_bestiary/django_bestiary/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('projects.urls')),
+    url(r'^', include('projects.urls')),
     url(r'^projects/', include('projects.urls'))
 ]
--- django_bestiary/projects/urls.py
+++ django_bestiary/projects/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^import/$', views.import_from_file),
     url(r'^export/$', views.export_to_file),
+    url(r'^editor/$', views.editor, name='editor'),
     url(r'^$', views.index, name='index'),
 ]
