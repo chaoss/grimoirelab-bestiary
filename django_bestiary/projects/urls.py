f6c05feec78edd1ae1af75d4925f85321cb425cc
Alvaro del Castillo <acs@bitergia.com>
2018-01-16 10:51:09 +0100

    [editor] Implement Add, Update and Remove in Repository Views

--- django_bestiary/projects/urls.py
+++ django_bestiary/projects/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     url(r'^remove_project$', views.remove_project),
     url(r'^select_project$', views.select_project),
     url(r'^select_data_source$', views.select_data_source),
+    url(r'^add_repository_view$', views.add_repository_view),
+    url(r'^remove_repository_view$', views.remove_repository_view),
     url(r'^select_repository_view$', views.select_repository_view),
     url(r'^update_repository_view$', views.update_repository_view),
     url(r'^$', views.index, name='index'),
