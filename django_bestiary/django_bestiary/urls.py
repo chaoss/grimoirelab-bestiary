d1417ab71062aab4d0f3c30a6f3bc22182663046
Alvaro del Castillo <acs@bitergia.com>
2018-02-02 13:47:23 +0100

    [custodian] The index for Bestiary project in custodian branch is the custodian app

--- django_bestiary/django_bestiary/urls.py
+++ django_bestiary/django_bestiary/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/projects')),
+    url(r'^$', RedirectView.as_view(url='/custodian')),
     url(r'^admin/', admin.site.urls),
     url(r'^projects/', include('projects.urls')),
     url(r'^custodian/', include('custodian.urls'))
