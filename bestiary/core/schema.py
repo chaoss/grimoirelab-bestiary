e8ffc8dc2843d7898557369a41b314a54deec3a4
Miguel Ángel Fernández <mafesan@bitergia.com>
2020-03-16 11:19:48 +0100

    [schema] Add basic authentication mutations
    These changes add support for performing authenticated queries
    (both GraphQL queries and mutations), based on JSON Web Tokens
    (JWT).
    
    To perform an authenticated query, an existing user must
    generate a token which has to be included in the `Authorization`
    header with the HTTP request. This token is generated using a
    mutation which comes defined by the `graphql-jwt` module,
    together with other extra mutations for verifying an existing
    token and refreshing it in case the token expired.
    
    By default, all the queries and mutations now require
    authentication. This is controlled by a custom decorator named
    `check_auth` which is based on the pre-defined decorator
    `login_required` and extending it to check both if the `user`
    object from the `context.info` object is authenticated and the
    value of a new control variable named `AUTHENTICATION_REQUIRED`,
    defined in Django settings allowing to set whether authentication
    is required or not.
    
    If the user is not logged or uses an invalid token, the following
    exception is raised:
    
    ```
    graphql.error.located_error.GraphQLLocatedError: You do not have
    permission to perform this action
    ```
    
    This commit also contains the basic configuration scripts both for
    developing and for testing. After these changes, the
    `AUTHENTICATION_REQUIRED` variable in the testing configuration
    script must be set to `True`.
    
    Signed-off-by: Miguel Ángel Fernández <mafesan@bitergia.com>

---
+++ bestiary/core/schema.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2020 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#     Miguel Ángel Fernández <mafesan@bitergia.com>
+#
+
+import graphene
+import graphql_jwt
+
+
+class BestiaryMutation(graphene.ObjectType):
+    """Mutations supported by Bestiary"""
+
+    # JWT authentication
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
