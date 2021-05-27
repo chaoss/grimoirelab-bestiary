6624b54f3b728332a6a0fbc072dc304c4612ed8b
Eva Millán <evamillan@bitergia.com>
2021-05-27 16:54:10 +0200

    [ui] Add mutations to edit project data
    Adds the 'updateProject' GraphQL mutation to
    update a project's metadata and the 'moveProject'
    one to move it to a different parent project.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

--- ui/tests/unit/mutations.spec.js
+++ ui/tests/unit/mutations.spec.js
@@ -6,7 +6,20 @@ import ProjectForm from "@/components/ProjectForm";
 
 Vue.use(Vuetify);
 
-describe("ProjectsForm queries", () => {
+describe("ProjectsForm mutations", () => {
+  const mountFunction = options => {
+    return shallowMount(ProjectForm, {
+      Vue,
+      Vuetify,
+      propsData: {
+        ecosystemId: 1,
+        getProjects: () => {},
+        saveFunction: () => {}
+      },
+      ...options
+    });
+  };
+
   const addProjectResponse = {
     data: {
       addProject: {
@@ -20,34 +33,83 @@ describe("ProjectsForm queries", () => {
     }
   };
 
-  test("Mock query for getProjects", async () => {
+  const updateProjectResponse = {
+    data: {
+      updateProject: {
+        project: {
+          id: "39",
+          name: "test",
+          __typename: "ProjectType"
+        },
+        __typename: "UpdateProject"
+      }
+    }
+  };
+
+  test("Mock mutation for addProject", async () => {
     const mutate = jest.fn(() => Promise.resolve(addProjectResponse));
-    const wrapper = shallowMount(ProjectForm, {
-      Vue,
+    const wrapper = mountFunction({
       mocks: {
         $apollo: {
           mutate
         }
       },
-      propsData: {
-        ecosystemId: 1,
-        getProjects: () => {},
-        addProject: mutate
-      },
       data() {
         return {
-          name: "New Project",
-          title: "new-project"
+          form: {
+            name: "New Project",
+            title: "new-project"
+          }
         };
       }
     });
+    await wrapper.setProps({ saveFunction: mutate });
     await Mutations.addProject(wrapper.vm.$apollo, {
-      name: wrapper.vm.name,
-      title: wrapper.vm.title,
+      name: wrapper.vm.form.name,
+      title: wrapper.vm.form.title,
       ecosystemId: wrapper.vm.ecosystemId
     });
 
     expect(mutate).toBeCalled();
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test("Mock mutation for updateProject", async () => {
+    const mutate = jest.fn(() => Promise.resolve(updateProjectResponse));
+    const wrapper = mountFunction({
+      mocks: {
+        $apollo: {
+          mutate
+        },
+        $router: {
+          push: () => {}
+        }
+      },
+      data() {
+        return {
+          form: {
+            name: "test",
+            title: "Test Title",
+            parentId: 2
+          }
+        };
+      }
+    });
+    await wrapper.setProps({ saveFunction: mutate });
+
+    //Mock Vuetify form validation
+    wrapper.vm.$refs.form.validate = () => {
+      return true;
+    };
+
+    await wrapper.vm.save();
+
+    expect(mutate).toBeCalledWith({
+      ecosystemId: 1,
+      name: "test",
+      parentId: 2,
+      title: "Test Title"
+    });
     expect(wrapper.element).toMatchSnapshot();
   });
 });
