4943c9b64b0184382cf3adad4a1293469ef470db
Eva Millán <evamillan@bitergia.com>
2021-06-22 11:30:13 +0200

    [ui] Add token and user information to the store
    Adds actions, getters and mutations to save and remove
    users names and tokens to the Vuex store.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -1,10 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Cookies from "js-cookie";
+import { tokenAuth } from "../apollo/mutations";
+import router from "../router";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    auth: {
+      token: Cookies.get("bestiary_authtoken"),
+      user: Cookies.get("bestiary_user")
+    },
     ecosystems: [],
     dialog: {
       isOpen: false,
@@ -20,6 +27,12 @@ export default new Vuex.Store({
     }
   },
   mutations: {
+    setToken(state, token) {
+      state.auth.token = token;
+    },
+    setUser(state, user) {
+      state.auth.user = user;
+    },
     setEcosystems(state, ecosystems) {
       state.ecosystems = ecosystems;
     },
@@ -44,6 +57,8 @@ export default new Vuex.Store({
     }
   },
   getters: {
+    isAuthenticated: state => !!state.auth.token,
+    user: state => state.auth.user,
     ecosystems: state => state.ecosystems,
     findEcosystem: state => id => {
       return state.ecosystems.find(
@@ -52,6 +67,33 @@ export default new Vuex.Store({
     },
     dialog: state => state.dialog,
     snackbar: state => state.snackbar
+  },
+  actions: {
+    async login({ commit }, authDetails) {
+      const response = await tokenAuth(
+        authDetails.apollo,
+        authDetails.username,
+        authDetails.password
+      );
+      if (response && !response.errors) {
+        const token = response.data.tokenAuth.token;
+        commit("setToken", token);
+        Cookies.set("bestiary_authtoken", token, { sameSite: "strict" });
+        commit("setUser", authDetails.username);
+        Cookies.set("bestiary_user", authDetails.username, {
+          sameSite: "strict"
+        });
+        return token;
+      }
+      return response;
+    },
+    logout({ commit }) {
+      commit("setToken", null);
+      commit("setUser", null);
+      Cookies.remove("bestiary_authtoken");
+      Cookies.remove("bestiary_user");
+      router.push("/login");
+    }
   },
   modules: {}
 });
